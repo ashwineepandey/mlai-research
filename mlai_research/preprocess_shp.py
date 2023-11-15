168ca3774459d79c8a791df628c0c6d56ff66de2
DaytaSigntist <ashwineep94@gmail.com>
2023-11-16 00:56:57 +0200

    update config, add new notebook and script for preprocessing shp files

---
+++ mlai_research/preprocess_shp.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('../mlai_research/')
+import log
+import utils
+import geopandas as gpd
+from shapely import wkb
+import pandas as pd
+
+logger = log.get_logger(__name__)
+
+def check_crs(gdf1, gdf2) -> bool: 
+    if gdf1.crs == gdf2.crs:
+        return True
+    else:
+        print("Error: The CRS for the shape files does not match.")
+        return False
+    
+    
+def load_shp_data(dir_path, filename1, filename2):
+    gdf1 = gpd.read_file(f"{dir_path}{filename1}") # import shapefile using geopandas
+    logger.info(f"{filename1} Shape: {gdf1.shape}")
+    gdf2 = gpd.read_file(f"{dir_path}{filename2}") # import shapefile using geopandas
+    logger.info(f"{filename2} Shape: {gdf2.shape}")
+    return gdf1, gdf2
+
+
+def select_subset_cols(df, subset_cols):
+    return df[subset_cols]
+
+
+def geom_drop_z_dim(df):
+    _drop_z = lambda geom: wkb.loads(wkb.dumps(geom, output_dimension=2))
+    df.geometry = df.geometry.transform(_drop_z)
+    return df
+
+
+def process_shp_files(gdf1, gdf2):
+    fil_gdf1 = select_subset_cols(gdf1, subset_cols=['Species', 'geometry'])
+    fil_gdf1 = geom_drop_z_dim(fil_gdf1)
+    fil_gdf2 = select_subset_cols(gdf2, subset_cols=['tag', 'geometry'])
+    fil_gdf2 = fil_gdf2.rename(columns={'tag': 'Species'})
+    return fil_gdf1, fil_gdf2
+
+
+def filter_relevant_species(df):
+    # logger.info(f"Species value counts (original label): {df['Species'].value_counts()}")
+    species_map = {"Xanthium strumarium" : "Xanthium",
+              "Datura stramonium": "Datura",
+               "Xanthium": "Xanthium",
+               "Datura": "Datura"}
+    df['Species'] = df['Species'].map(species_map).fillna('Other')
+    logger.info(f'Filtered Species value counts: {df.Species.value_counts()}')
+    return df
+
+
+def sync_crs(gdf, rasterimg) -> bool: 
+    if gdf.crs != rasterimg.crs:
+        gdf = gdf.set_crs(str(rasterimg.crs))
+    return gdf
+
+
+def main():
+    conf = utils.load_config("base")
+    gdf1, gdf2 = load_shp_data(f"{conf.data.path_raw}classification_points/", conf.data.fn_shp_raw1, conf.data.fn_shp_raw2)
+    if check_crs(gdf1, gdf2):
+        fil_gdf1, fil_gdf2 = process_shp_files(gdf1, gdf2)
+        comb_gdf = pd.concat([fil_gdf1, fil_gdf2]).reset_index(drop=True)
+        logger.info(f"Combined Shape: {comb_gdf.shape}")
+        fil_comb_gdf = filter_relevant_species(comb_gdf)
+        fil_comb_gdf['pid'] = list(range(len(fil_comb_gdf)))
+        fil_comb_gdf.to_file(f"{conf.data.path_base_points}{conf.data.fn_shp_combined}", driver='ESRI Shapefile')
--- mlai_research/utils.py
+++ mlai_research/utils.py
@@ -1,15 +1,15 @@
 
 
-import sys
-import yaml
 import log
+import os
 import functools
 import time
+import glob
 from pyhocon import ConfigFactory
 from typing import Dict, List, Tuple
 from datetime import datetime
-import pandas as pd
-import yaml
+import rasterio
+import matplotlib.pyplot as plt
 
 logger = log.get_logger(__name__)
 
@@ -34,3 +34,42 @@ def load_config(fn):
     Load the configuration file from a hocon file object and returns it (https://github.com/chimpler/pyhocon).
     """
     return ConfigFactory.parse_file(f"../config/{fn}.conf")
+
+
+def save_plot(fig, filename):
+    fig.savefig(filename)
+
+
+def load_raster(path):
+    # Load raster data
+    img = rasterio.open(path)
+    logger.info(f"Loaded image: {path}")
+    logger.info(f"Image channels: {img.count}")
+    logger.info(f"Image size: {img.width}x{img.height}")
+    logger.info(f"Image crs: {img.crs}")
+    logger.info(f"Image bounds: {img.bounds}")
+    logger.info(f"Image transform: {img.transform}")
+    return img
+
+
+def get_filenames(path, ext):
+    return glob.glob(f'{path}*.{ext}')
+
+
+def plot_cropped_tifs(cropped_tifs, title):
+    ncols = 3 # set number of columns (use 3 to demonstrate the change)
+    nrows = len(cropped_tifs) // ncols + (len(cropped_tifs) % ncols > 0) # calculate number of rows
+
+    plt.figure(figsize=(15, 20))
+    plt.suptitle(title, fontsize=18, y=0.95)
+
+    for n, file in enumerate(cropped_tifs):
+        # add a new subplot iteratively using nrows and cols
+        ax = plt.subplot(nrows, ncols, n + 1)
+        # Plot raster crop
+        saved_clipped_raster_io = rasterio.open(f"{file}")
+        rasterio.plot.show(saved_clipped_raster_io, ax=ax)
+        # chart formatting
+        ax.set_title(os.path.basename(file), fontsize=8)
+        ax.axis('off')
+    plt.show()
